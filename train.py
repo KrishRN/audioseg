193d356a8a22560aee2424716c6014bca78161e0
Krishanthan <krisharun1997@gmail.com>
2021-11-12 00:14:28 +0530

    Added Transformer Encoder

--- train.py
+++ train.py
@@ -8,43 +8,197 @@ import argparse
 from tensorflow.keras import utils, models, layers
 from tensorflow.keras.callbacks import ModelCheckpoint
 
-session_conf = tf.compat.v1.ConfigProto(intra_op_parallelism_threads=10,inter_op_parallelism_threads=10)
+session_conf = tf.compat.v1.ConfigProto(intra_op_parallelism_threads=10, inter_op_parallelism_threads=10)
 sess = tf.compat.v1.Session(config=session_conf)
 
+
 # Define Model
+def get_angles(pos, i, d_model):
+    angle_rates = 1 / np.power(10000, (2 * (i // 2)) / np.float32(d_model))
+    return pos * angle_rates
+
+
+def positional_encoding(position, d_model):
+    angle_rads = get_angles(np.arange(position)[:, np.newaxis],
+                            np.arange(d_model)[np.newaxis, :],
+                            d_model)
+
+    # apply sin to even indices in the array; 2i
+    angle_rads[:, 0::2] = np.sin(angle_rads[:, 0::2])
+
+    # apply cos to odd indices in the array; 2i+1
+    angle_rads[:, 1::2] = np.cos(angle_rads[:, 1::2])
+
+    pos_encoding = angle_rads[np.newaxis, ...]
+
+    return tf.cast(pos_encoding, dtype=tf.float32)
+
+
+class MultiHeadAttention(tf.keras.layers.Layer):
+    def __init__(self, d_model=128, num_heads=8, causal=False, dropout=0.0):
+        super().__init__()
+
+        assert d_model % num_heads == 0
+        depth = d_model // num_heads
+
+        self.w_query = tf.keras.layers.Dense(d_model)
+        self.split_reshape_query = tf.keras.layers.Reshape((-1, num_heads, depth))
+        self.split_permute_query = tf.keras.layers.Permute((2, 1, 3))
+
+        self.w_value = tf.keras.layers.Dense(d_model)
+        self.split_reshape_value = tf.keras.layers.Reshape((-1, num_heads, depth))
+        self.split_permute_value = tf.keras.layers.Permute((2, 1, 3))
+
+        self.w_key = tf.keras.layers.Dense(d_model)
+        self.split_reshape_key = tf.keras.layers.Reshape((-1, num_heads, depth))
+        self.split_permute_key = tf.keras.layers.Permute((2, 1, 3))
+
+        self.attention = tf.keras.layers.Attention(causal=causal, dropout=dropout)
+        self.join_permute_attention = tf.keras.layers.Permute((2, 1, 3))
+        self.join_reshape_attention = tf.keras.layers.Reshape((-1, d_model))
+
+        self.dense = tf.keras.layers.Dense(d_model)
+
+    def call(self, inputs, mask=None, training=None):
+        q = inputs[0]
+        v = inputs[1]
+        k = inputs[2] if len(inputs) > 2 else v
+
+        query = self.w_query(q)
+        query = self.split_reshape_query(query)
+        query = self.split_permute_query(query)
+
+        value = self.w_value(v)
+        value = self.split_reshape_value(value)
+        value = self.split_permute_value(value)
+
+        key = self.w_key(k)
+        key = self.split_reshape_key(key)
+        key = self.split_permute_key(key)
+
+        if mask is not None:
+            if mask[0] is not None:
+                mask[0] = tf.keras.layers.Reshape((-1, 1))(mask[0])
+                mask[0] = tf.keras.layers.Permute((2, 1))(mask[0])
+            if mask[1] is not None:
+                mask[1] = tf.keras.layers.Reshape((-1, 1))(mask[1])
+                mask[1] = tf.keras.layers.Permute((2, 1))(mask[1])
+
+        attention = self.attention([query, value, key], mask=mask)
+        attention = self.join_permute_attention(attention)
+        attention = self.join_reshape_attention(attention)
+
+        x = self.dense(attention)
+
+        return x
+
+
+class EncoderLayer(tf.keras.layers.Layer):
+    def __init__(self, d_model=128, num_heads=8, dff=2048, dropout=0.0):
+        super().__init__()
+
+        self.multi_head_attention = MultiHeadAttention(d_model, num_heads)
+        self.dropout_attention = tf.keras.layers.Dropout(dropout)
+        self.add_attention = tf.keras.layers.Add()
+        self.layer_norm_attention = tf.keras.layers.LayerNormalization(epsilon=1e-6)
+
+        self.dense1 = tf.keras.layers.Dense(dff, activation='relu')
+        self.dense2 = tf.keras.layers.Dense(d_model)
+        self.dropout_dense = tf.keras.layers.Dropout(dropout)
+        self.add_dense = tf.keras.layers.Add()
+        self.layer_norm_dense = tf.keras.layers.LayerNormalization(epsilon=1e-6)
+
+    def call(self, inputs, mask=None, training=None):
+        # print(mask)
+        attention = self.multi_head_attention([inputs, inputs, inputs], mask=[mask, mask])
+        attention = self.dropout_attention(attention, training=training)
+        x = self.add_attention([inputs, attention])
+        x = self.layer_norm_attention(x)
+        # x = inputs
+
+        ## Feed Forward
+        dense = self.dense1(x)
+        dense = self.dense2(dense)
+        dense = self.dropout_dense(dense, training=training)
+        x = self.add_dense([x, dense])
+        x = self.layer_norm_dense(x)
+
+        return x
+
+
+class Encoder(tf.keras.layers.Layer):
+    def __init__(self,  num_layers=4, d_model=128, num_heads=8, dff=2048,
+                 maximum_position_encoding=10000, dropout=0.0):
+        super().__init__()
+
+        self.d_model = d_model
+
+        self.pos = positional_encoding(maximum_position_encoding, d_model)
+
+        self.encoder_layers = [EncoderLayer(d_model=d_model, num_heads=num_heads, dff=dff, dropout=dropout) for _ in
+                               range(num_layers)]
+
+        self.dropout = tf.keras.layers.Dropout(dropout)
+
+    def call(self, inputs, mask=None, training=None):
+        x = self.embedding(inputs)
+        # positional encoding
+        x *= tf.math.sqrt(
+            tf.cast(self.d_model, tf.float32))  # scaling by the sqrt of d_model, not sure why or if needed??
+        x += self.pos[:, :tf.shape(x)[1], :]
+
+        x = self.dropout(x, training=training)
+
+        # Encoder layer
+        embedding_mask = self.embedding.compute_mask(inputs)
+        for encoder_layer in self.encoder_layers:
+            x = encoder_layer(x, mask=embedding_mask)
+
+        return x
+
+    def compute_mask(self, inputs, mask=None):
+        return self.embedding.compute_mask(inputs)
+
+    def get_config(self):
+        config = super().get_config()
+        config.update({"d_model": self.d_model})
+        return config
+
+
 def cnn_bilstm(output_layer_width):
     model = models.Sequential()
     model.add(layers.TimeDistributed(layers.Conv2D(64, (5, 5), activation='elu'), input_shape=(None, 32, 32, 1)))
-    model.add(layers.TimeDistributed(layers.MaxPooling2D((2,2))))
+    model.add(layers.TimeDistributed(layers.MaxPooling2D((2, 2))))
     model.add(layers.TimeDistributed(layers.Conv2D(128, (3, 3), activation='elu')))
-    model.add(layers.TimeDistributed(layers.MaxPooling2D((2,2))))
+    model.add(layers.TimeDistributed(layers.MaxPooling2D((2, 2))))
     model.add(layers.TimeDistributed(layers.Conv2D(128, (3, 3), activation='elu')))
-    model.add(layers.TimeDistributed(layers.MaxPooling2D((2,2))))
+    model.add(layers.TimeDistributed(layers.MaxPooling2D((2, 2))))
     model.add(layers.TimeDistributed(layers.Flatten()))
     model.add(layers.TimeDistributed(layers.Dense(128, activation='elu')))
     model.add(layers.Dropout(0.5))
-    model.add(layers.Bidirectional(layers.LSTM(128, return_sequences=True)))
+    model.add(Encoder( num_layers = 4, d_model = 128, num_heads = 8, dff = 2048, dropout = 0.0))
     model.add(layers.Dropout(0.5))
     model.add(layers.TimeDistributed(layers.Dense(output_layer_width, activation='softmax')))
     model.compile(optimizer='adam',
-                loss='binary_crossentropy',
-                metrics=['accuracy'])
+                  loss='binary_crossentropy',
+                  metrics=['accuracy'])
     return model
 
+
 # Define training parameters
-def train_model(model, x_train, y_train, validation_split, x_dev=None, y_dev=None, epochs=25, batch_size=64, callbacks=None):
+def train_model(model, x_train, y_train, validation_split, x_dev=None, y_dev=None, epochs=25, batch_size=64,
+                callbacks=None):
     if validation_split:
-        return model.fit(x_train[:,:,:,:,np.newaxis], y_train, validation_split = validation_split,
-                     epochs=epochs, batch_size=batch_size, callbacks=callbacks)
+        return model.fit(x_train[:, :, :, :, np.newaxis], y_train, validation_split=validation_split,
+                         epochs=epochs, batch_size=batch_size, callbacks=callbacks)
     elif x_dev is not None:
-        return model.fit(x_train[:,:,:,:,np.newaxis], y_train,
-                     validation_data=(x_dev[:,:,:,:,np.newaxis], y_dev),
-                     epochs=epochs, batch_size=batch_size, callbacks=callbacks)
+        return model.fit(x_train[:, :, :, :, np.newaxis], y_train,
+                         validation_data=(x_dev[:, :, :, :, np.newaxis], y_dev),
+                         epochs=epochs, batch_size=batch_size, callbacks=callbacks)
     else:
         print('WARNING: no validation data, or validation split provided.')
-        return model.fit(x_train[:,:,:,:,np.newaxis], y_train,
-                     epochs=epochs, batch_size=batch_size)
-
+        return model.fit(x_train[:, :, :, :, np.newaxis], y_train,
+                         epochs=epochs, batch_size=batch_size)
 
 
 if __name__ == '__main__':
@@ -109,5 +263,5 @@ if __name__ == '__main__':
         df.index.name = 'epoch'
         df.to_csv(f'{args.out_dir}/{args.model_name}_training_log.csv')
     else:
-        print(f'ERROR: Number of classes {y.shape[-1]} is not equal to 2 or 4, see README for more info on using this training script.')
-        
+        print(
+            f'ERROR: Number of classes {y.shape[-1]} is not equal to 2 or 4, see README for more info on using this training script.')
