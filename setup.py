120cd833bc8f0035daf384a6e01735fbd958dcd4
Krishanthan <krisharun1997@gmail.com>
2021-11-11 19:36:15 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,21 +3,15 @@ from setuptools import find_packages, setup
 with open("README.md") as f:
     readme = f.read()
 
-with open('LICENSE') as f:
-    license = f.read()
-
 setup(
-    name="voxseg",
+    name="audioseg",
     version="0.1.0",
-    author="Nick Wilkinson",
-    author_email="nwilkinsonza@gmail.com",
-    description="Python voice activity detection package",
+    author="Krishanthan",
+    description="Python Audio Segmentation ",
     long_description=readme,
-    url="https://github.com/NickWilkinson37/voxseg",
+    url="https://github.com/KrishRN/AudioSegmentation",
     packages=find_packages(exclude=("tests",)),
-    license=license,
     classifiers=[
-        'License :: OSI Approved :: MIT License',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
